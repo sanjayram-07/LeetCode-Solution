c64a9fcf36285bedca4a9810f7216ab10674a46f
Sanjay Ram M <m.sanjayram07@gmail.com>
2025-09-18 19:28:56 +0530

    0 ms (100%) 0009-palindrome-number 09-18-2025 .cpp

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+ int reverse(int x) {
+        int ans =0 ;
+        
+        while(x!=0)
+        {
+            int digit =x%10;
+              if(ans>INT_MAX/10 || ans< INT_MIN/10)
+            {
+                return 0;
+            }
+            ans = (ans*10)+digit;
+          
+            x/=10;
+        }
+        
+        return ans;
+
+        
+    }
+    bool isPalindrome(int xn) {
+        if(xn<0) return false;
+       return(reverse(xn)== xn);
+        
+
+        
+    }
+};
